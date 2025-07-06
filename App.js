73bb39f9d826431efc062b65b2cd6ff81b29ffc6
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-07 02:38:34 +0600

    file initialized , native wind added , react navigation added

---
+++ App.js
@@ -0,0 +1,14 @@
+import './global.css';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <View>
+        <Text className="bg-green-700 font-bold uppercase underline">
+          Polash is a RN Developer InshaAllah
+        </Text>
+      </View>
+    </>
+  );
+}
