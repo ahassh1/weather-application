d50466dddfc1180aa0016c149aa760b4d46abe78
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-08 10:33:47 +0600

    search bar 1st part

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,44 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import { theme } from 'theme';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text className=" flex-1 items-center justify-center  p-10 font-bold uppercase text-red-800">
-        HomeScreen
-      </Text>
-    </SafeAreaView>
+    <View className="relative flex-1">
+      <StatusBar style="light" />
+      <Image
+        blurRadius={5}
+        source={require('../assets/images/bgdone.png')}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        {/* search section  */}
+        <View style={{ height: '7%' }} className="relative z-50 mx-4">
+          <View
+            className="flex-row items-center justify-end rounded-full"
+            style={{ backgroundColor: theme.bgWhite(0.2) }}>
+            <TextInput
+              placeholder="Search City"
+              placeholderTextColor={'lightgray'}
+              className="relative h-10 flex-1 border border-gray-50 pl-6 text-base text-white"
+            />
+            <TouchableOpacity
+              style={{ backgroundColor: theme.bgWhite(0.3) }}
+              className="m-1 rounded-full p-3"
+            />
+            <MagnifyingGlassIcon size={25} color="white" className="absolute" />
+          </View>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
