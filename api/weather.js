0758cbb086561d65ecdd109866baeaee5c255fa8
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-09 14:28:11 +0600

    fetch the forecast dat

--- api/weather.js
+++ api/weather.js
@@ -20,3 +20,11 @@ const apiCall = async (endpoint) => {
     return null;
   }
 };
+
+export const fetchWeatherForecast = (params) => {
+  return apiCall(forecastEndpoint(params));
+};
+
+export const fetchLocations = (params) => {
+  return apiCall(locationsEndpoint(params));
+};
